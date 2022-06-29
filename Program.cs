18d7f9256340fac0fe9a8cc69c1b6188ae5c8647
James Grieve <jgrieve@ualberta.ca>
2022-06-29 08:23:44 -0600

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿public bool TestValidEmail(string email)
+// Tests to ensure the phone number entered follows the format mask "text@text.text".  Returns true if so, and false if not.
+{
+    return 
+}
+public bool TestValidPhoneNumber(string phone)
+// Tests to ensure the phone number entered follows the format mask "###-###-####".  Returns true if so, and false if not.
+    {
+
+}
+public bool TestValidNumber(int number)
+// Tests to ensure the number entered is between 1 and 100 inclusive. Returns true if so, and false if not.
+{
+
+}
